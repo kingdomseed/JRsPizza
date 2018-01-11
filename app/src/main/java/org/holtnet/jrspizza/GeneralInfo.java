20b82585ead19b02b09a748439ac0edfc9960cd3
Jason M Holt <kingdomseed@gmail.com>
2018-01-11 09:00:17 -0600

    First upload of entire project. (Learning this as I go... lol)

---
+++ app/src/main/java/org/holtnet/jrspizza/GeneralInfo.java
@@ -0,0 +1,13 @@
+package org.holtnet.jrspizza;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GeneralInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(org.holtnet.jrspizza.R.layout.activity_general_info);
+    }
+}
